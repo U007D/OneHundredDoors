17185f07cd383f34622667f11cb7309321db5feb
bRad <brad@humanenginuity.com>
2017-03-01 18:30:28 -0800

    Made fully declarative

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-//#![cfg_attr(feature="clippy", feature(plugin))]
-//#![cfg_attr(feature="clippy", plugin(clippy))]
-//#![deny(missing_docs, missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts,
-//unsafe_code, unstable_features, unused_import_braces, unused_qualifications)]
-//#![allow(non_snake_case)]
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#![deny(missing_debug_implementations, missing_copy_implementations, trivial_casts, trivial_numeric_casts,
+unsafe_code, unused_import_braces, unused_qualifications)]
+#![allow(non_snake_case)]
 #![feature(inclusive_range_syntax)]
 
-extern crate itertools;
-use itertools::Itertools;
-
 fn main() {
-    let mut doors = vec![false; 100];
-
-    let _ = (1...100).into_iter()
-             .map(|pass| doors.iter_mut().step(pass)
-                              .map(|door| *door = !*door ).last()).last();
+    let doors = vec![false; 100].iter_mut().enumerate()
+                                .map(|(door, door_state)| (1...100).into_iter()
+                                                                   .map(|pass| match door % pass {
+                                                                        0 => { *door_state = !*door_state; *door_state },
+                                                                        _ => *door_state })
+                                                                   .last().unwrap()).collect::<Vec<_>>();
 
     println!("{:?}", doors);
 }
